52933eaf7f7240b5e55c76f472e6daa125a4463e
Luis Puig <lpuig89@gmail.com>
2023-12-07 11:55:29 +0100

    Fix test for sonar

--- src/controllers/users.controller.test.ts
+++ src/controllers/users.controller.test.ts
@@ -1,13 +1,16 @@
 import { NextFunction, Request, Response } from 'express';
 import { UsersController } from './users.controller';
 import { UsersMongoRepo } from '../repos/users/users.mongo.repo';
+import { User } from '../entities/user';
 
 describe('Given UsersController class', () => {
   let controller: UsersController;
   let mockRequest: Request;
   let mockResponse: Response;
   let mockNext: NextFunction;
-  beforeEach(() => {
+  let mockRepo: jest.Mocked<UsersMongoRepo>;
+
+  beforeAll(() => {
     mockRequest = {
       body: {},
       params: {},
@@ -19,16 +22,19 @@ describe('Given UsersController class', () => {
     } as unknown as Response;
     mockNext = jest.fn();
   });
-  describe('When we instantiate it without errors', () => {
-    beforeEach(() => {
-      const mockRepo = {
-        getAll: jest.fn().mockResolvedValue([{}]),
-        getById: jest.fn().mockResolvedValue({}),
-        create: jest.fn().mockResolvedValue({}),
-      } as unknown as UsersMongoRepo;
-      controller = new UsersController(mockRepo);
-    });
 
+  beforeEach(() => {
+    mockRepo = {
+      getAll: jest.fn().mockResolvedValue([{}]),
+      getById: jest.fn().mockResolvedValue({}),
+      create: jest.fn().mockResolvedValue({}),
+      login: jest.fn().mockResolvedValue({}),
+    } as unknown as jest.Mocked<UsersMongoRepo>;
+
+    controller = new UsersController(mockRepo);
+  });
+
+  describe('When we instantiate it without errors', () => {
     test('Then getAll should ...', async () => {
       await controller.getAll(mockRequest, mockResponse, mockNext);
       expect(mockResponse.json).toHaveBeenCalledWith([{}]);
@@ -41,6 +47,29 @@ describe('Given UsersController class', () => {
     test('Then create should ...', async () => {
       await controller.create(mockRequest, mockResponse, mockNext);
       expect(mockResponse.json).toHaveBeenCalledWith({});
+    });
+
+    test('Then login should successfully authenticate with valid credentials and return user data and token', async () => {
+      const mockRequest = {
+        body: {
+          email: 'test@example.com',
+          passwd: 'test',
+        },
+      } as unknown as Request;
+
+      const mockUser = {
+        email: 'TestName',
+        passwd: 'test',
+      } as unknown as User;
+
+      mockRepo.login.mockResolvedValueOnce(mockUser);
+      await controller.login(mockRequest, mockResponse, mockNext);
+
+      expect(mockRepo.login).toHaveBeenCalledWith({
+        email: 'test@example.com',
+        passwd: 'test',
+      });
+      expect(mockResponse.status).toHaveBeenCalledWith(202);
     });
   });
 
@@ -77,31 +106,13 @@ describe('Given UsersController class', () => {
       expect(mockNext).toHaveBeenLastCalledWith(mockError);
     });
 
-    test('Then login should...', async () => {
-      const mockUserId = 'mockUserId';
-      const mockLoginResult = { id: 'mockUserId', email: 'mock@example.com' };
-
-      // Mocking the request with a userId
-      const mockRequest = {
-        body: { userId: mockUserId },
-      } as unknown as Request;
-
-      // Mocking the repo methods for both cases
-      const mockRepo = {
-        getById: jest.fn().mockResolvedValue(mockLoginResult),
-        login: jest.fn().mockResolvedValue(mockLoginResult),
-      } as unknown as UsersMongoRepo;
-
-      // Creating two instances of the controller
-      const controller = new UsersController(mockRepo);
-
-      // Testing with userId
+    test('Then login should throw an error', async () => {
       await controller.login(mockRequest, mockResponse, mockNext);
-      expect(mockRepo.getById).toHaveBeenCalledWith(mockUserId);
-      expect(mockResponse.json).toHaveBeenCalledWith({
-        user: mockLoginResult,
-        token: expect.any(String),
-      });
+      expect(mockNext).toHaveBeenCalledWith(mockError);
+    });
+    test('Then register (create) should throw an error', async () => {
+      await controller.create(mockRequest, mockResponse, mockNext);
+      expect(mockNext).toHaveBeenCalledWith(mockError);
     });
   });
 });
